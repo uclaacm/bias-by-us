3c0d419a062fb7bfc3083ecad6941a4d027d7e82
Jiin <jiinkim02@gmail.com>
2020-09-18 09:32:33 -0700

    Added Requested Changes
    - only used classes, and all classes are lowercase-dash-cased
    
    - changed teachla logo from PNG to SVG, moved the assets file into src so that the logo can be imported on App.js
    
    - simplifying code
    
    - other requested changes in css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import './AppMain.css';
+import teachLogo from './assets/teachla-logo.svg'
 
 function App() {
 
@@ -11,54 +12,40 @@ function App() {
 
   // dropdownHandler: arrow function that switches dropdownOpen value whenever the button is toggled
   const dropdownHandler = () => {
-    if (dropdownOpen === false) {
-      setDropdownOpen(true);
-    } else {
-      setDropdownOpen(false);
-    }
+    setDropdownOpen(!dropdownOpen)
   }
 
   // dropdownArea: arrow function that returns the apporpriate button (and dropdown menu if applicable) depending on whether dropdownOpen is true/false
   const dropdownArea = () => {
-    if (dropdownOpen === false) {
-      return (
-        <button onClick={dropdownHandler}>
-          <div className="triangle"></div>
-        </button>
-      );
-    } else {
-      return (
-        <button onClick={dropdownHandler}>
-          <div className="triangle up"></div>
-          {/* below is a sample Dropdown Menu placeholder -- this is where the actual component will go */}
-          <div id='dropdownPlaceholder'>dropdown menu</div>
-        </button>
-        
-      );
-    }
+    return (
+      <button className='button' onClick={dropdownHandler}>
+        <div className='triangle' {...dropdownOpen ? "up" : ""}></div>
+        {dropdownOpen && <div className='dropdown-placeholder'>dropdown menu</div>}
+      </button>
+    );
   }
 
   return (
-    <div className='App'>
+    <div className='app'>
 
-      <div className='NavBar'>
+      <div className='navbar'>
         {/* the TeachLA logo will be a link to ACM TeachLA's website*/ }
-        <a href='https://teachla.uclaacm.com'><img src='assets/TeachLA.PNG' alt='TeachLA icon' id='TeachLA'/></a>
+        <a href='https://teachla.uclaacm.com'><img src={teachLogo} alt='TeachLA icon' className='logo'/></a>
         {/* 'Bias by Us' will be a link -- clicking on it will go to the introduction page of our learning lab */}
-        <h1 id='BiasByUs'>Bias by Us</h1>
-        <h2 id='currentCaseStudy'>Case Study #2: Facebook Ads</h2>
+        <h1 className='title'>Bias by Us</h1>
+        <h2 className='subtitle'>Case Study #2: Facebook Ads</h2>
         {dropdownArea()}
       </div>
 
       {/* below are Progress Bar and Direction Buttons placeholders*/}
-      <div className='ProgressBar'>progress bar</div>
-      <div className='DirectionButtons'>direction buttons</div>
+      <div className='progress-bar'>progress bar</div>
+      <div className='direction-buttons'>direction buttons</div>
 
-      <div className='Posts'>
+      <div className='posts'>
         {/* below are sample Post placeholders */}
-        <p>post 1</p>
-        <p>post 2</p>
-        <p>post 3</p>
+        <p className='sample-post'>post 1</p>
+        <p className='sample-post'>post 2</p>
+        <p className='sample-post'>post 3</p>
         {/* this mapping function will render the actual list of Posts */}
         {
           /*
