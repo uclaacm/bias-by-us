be78462240c461cb520322ec174f46d0103888c6
Jiin <jiinkim02@gmail.com>
2021-01-18 22:22:23 -0800

    adds aria-label for refresh button

--- src/components/mainContent/StereotypeActivity.js
+++ src/components/mainContent/StereotypeActivity.js
@@ -104,7 +104,7 @@ export default function ConclusionGraphic() {
         return (
             <div>
                 <p className="stereotype-message">These are the genders that are stereotypically associated with each occupation. How close to these stereotypes were your predictions?</p>
-                <button className="stereotype-refresh-button" onClick={finishedHandler}><img alt="click to restart" src={restartButton}/></button>
+                <button aria-label="click to restart" className="stereotype-refresh-button" onClick={finishedHandler}><img src={restartButton}/></button>
             </div>
         );
     }
