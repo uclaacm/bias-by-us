222a0cd07f7a4c4b9089301e6c6dd752618451e2
Matthew Nieva <65370631+matthewcn56@users.noreply.github.com>
2021-05-25 20:10:48 -0700

    Restructured Code Instead of Memoization (#40)
    * Restructured code instead of memoization
    
    * Fixed cleanup effects

--- src/caseStudies/CaseStudyFacebook.js
+++ src/caseStudies/CaseStudyFacebook.js
@@ -2,26 +2,20 @@ import React, { useState } from "react";
 import "../AppMain.css";
 import PostsList from "../components/mainContent/PostsList";
 import labContent from "../assets/labContent";
-import { useWindowDimensions } from "../components/mainContent/commonLogic";
 import ProgressBar from "../components/mainContent/ProgressBar";
-
 function CaseStudyFacebook() {
   const [visibleSections, setVisibleSections] = useState(0);
   const [currentVisibleText, setCurrentVisibleText] = useState(0);
   const [forwardVisible, setForwardVisible] = useState(false);
-  const [windowWidth] = useWindowDimensions();
-
   return (
     <div className="app">
-      {windowWidth > 1300 && (
-        <ProgressBar
-          content={labContent}
-          visibleSections={visibleSections}
-          setVisibleSections={setVisibleSections}
-          setForwardVisible={setForwardVisible}
-          setCurrentVisibleText={setCurrentVisibleText}
-        />
-      )}
+      <ProgressBar
+        content={labContent}
+        visibleSections={visibleSections}
+        setVisibleSections={setVisibleSections}
+        setForwardVisible={setForwardVisible}
+        setCurrentVisibleText={setCurrentVisibleText}
+      />
 
       <div className="posts">
         <PostsList
--- src/components/mainContent/ProgressBar.js
+++ src/components/mainContent/ProgressBar.js
@@ -3,32 +3,48 @@ import "../../AppMain.css";
 import "../../index.css";
 import "../posts/posts.css";
 import "./mainContent.css";
-import {Link} from "react-scroll";
+import { useWindowDimensions } from "./commonLogic";
+import { Link } from "react-scroll";
 
 export default function ProgressBar(props) {
+  const [windowWidth] = useWindowDimensions();
 
-    const indices = [0, 1, 3, 5, 7, 9];
-    let content = props.content.filter((s, index) => indices.includes(index))
-        .map((section, index) => {
-            return (
-                /* scrollTo configuration options:
-                   - smooth: animates the scrolling with smooth movement
-                   - offset: scroll additional px (like padding), so that we can see the content under the navbar
-                   - duration: total duration of the scroll animation
-                */
-                <Link activeClass="active" to={section.post.header} smooth={true} offset={-100} duration={500} className='progress-button' key ={index} aria-label={`move to ${section.post.header}`}>
-                    <img 
-                        className={`profile-pic no-margin static-size ${props.content.indexOf(section) > props.visibleSections ? "gray" : ""}`} 
-                        src={section.post.profilePic} 
-                        alt={`the icon for the "${section.post.header}" section`}
-                    />
-                </Link>
-            );
-        });
+  const indices = [0, 1, 3, 5, 7, 9];
+  let content = props.content
+    .filter((s, index) => indices.includes(index))
+    .map((section, index) => {
+      return (
+        /* scrollTo configuration options:
+            - smooth: animates the scrolling with smooth movement
+            - offset: scroll additional px (like padding), so that we can see the content under the navbar
+            - duration: total duration of the scroll animation
+        */
+        <Link
+          activeClass="active"
+          to={section.post.header}
+          smooth={true}
+          offset={-100}
+          duration={500}
+          className="progress-button"
+          key={index}
+          aria-label={`move to ${section.post.header}`}
+        >
+          <img
+            className={`profile-pic no-margin static-size ${
+              props.content.indexOf(section) > props.visibleSections
+                ? "gray"
+                : ""
+            }`}
+            src={section.post.profilePic}
+            alt={`the icon for the "${section.post.header}" section`}
+          />
+        </Link>
+      );
+    });
 
-    return (
-        <div className="progress-bar">
-            {content} 
-        </div>
-    );
+  /*conditionally render either null or the progress bar 
+  depending on the window width */
+  return windowWidth > 1300 ? (
+    <div className="progress-bar">{content}</div>
+  ) : null;
 }
