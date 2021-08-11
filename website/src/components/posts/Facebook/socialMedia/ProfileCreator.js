65f42a4078853333fb415e3a362cc90d54ccc4b4
Jiin Kim <jiinkim02@gmail.com>
2021-08-11 11:01:48 -0700

    Social media ad component (#53)
    * implements ad component to social media section
    
    * removed unnecessary React import
    
    * addressing requested changes + made button size responsive

--- website/src/components/posts/Facebook/socialMedia/ProfileCreator.js
+++ website/src/components/posts/Facebook/socialMedia/ProfileCreator.js
@@ -44,82 +44,86 @@ export default function ProfileCreator(props) {
   }
 
   return (
-    <div className="flex-column">
+    <div>
       <div className="iphone-top"></div>
-      <div className="profile-header" id="user-profile">
-        Generated Profile
-      </div>
-      <div>
-        <img
-          className="user-profile-pic"
-          src={joeBruin}
-          alt="Joe Bruin, UCLA's mascot"
-        />
-      </div>
-      <div>
-        <div> Gender </div>
+      <div className="iphone-screen">
+        <div className="profile-header" id="user-profile">
+          Generated Profile
+        </div>
+        <div>
+          <img
+            className="user-profile-pic"
+            src={joeBruin}
+            alt="Joe Bruin, UCLA's mascot"
+          />
+        </div>
+        <div>
+          <div> Gender </div>
+          <div>
+            <button
+              className={
+                /* conditionally render what button appears*/
+                `default-button ${
+                  genderInput === "male" ? "male-button-active" : "hover-button"
+                } `
+              }
+              value="male"
+              onClick={handleGenderClick}
+            >
+              Male
+            </button>
+            <button
+              className={`default-button ${
+                genderInput === "female"
+                  ? "female-button-active"
+                  : "hover-button"
+              } `}
+              value="female"
+              onClick={handleGenderClick}
+            >
+              Female
+            </button>
+          </div>
+        </div>
+        <div>
+          <div> Race </div>
+          <div>
+            <button
+              className={`default-button ${
+                raceInput === "white" ? "white-button-active" : "hover-button"
+              } `}
+              value="white"
+              onClick={handleRaceClick}
+            >
+              White
+            </button>
+            <button
+              className={`default-button ${
+                raceInput === "black" ? "black-button-active" : "hover-button"
+              } `}
+              value="black"
+              onClick={handleRaceClick}
+            >
+              Black
+            </button>
+          </div>
+        </div>
         <div>
           <button
-            className={
-              /* conditionally render what button appears*/
-              `default-button ${
-                genderInput === "male" ? "male-button-active" : "hover-button"
-              } `
-            }
-            value="male"
-            onClick={handleGenderClick}
+            className="default-button long-button hover-button"
+            onClick={generateProfile}
           >
-            Male
-          </button>
-          <button
-            className={`default-button ${
-              genderInput === "female" ? "female-button-active" : "hover-button"
-            } `}
-            value="female"
-            onClick={handleGenderClick}
-          >
-            Female
+            Generate Profile
           </button>
         </div>
-      </div>
-      <div>
-        <div> Race </div>
         <div>
           <button
-            className={`default-button ${
-              raceInput === "white" ? "white-button-active" : "hover-button"
-            } `}
-            value="white"
-            onClick={handleRaceClick}
+            className="default-button long-button hover-button"
+            onClick={resetProfile}
           >
-            White
-          </button>
-          <button
-            className={`default-button ${
-              raceInput === "black" ? "black-button-active" : "hover-button"
-            } `}
-            value="black"
-            onClick={handleRaceClick}
-          >
-            Black
+            Reset Profile
           </button>
         </div>
-      </div>
-      <div>
-        <button
-          className="default-button long-button hover-button"
-          onClick={generateProfile}
-        >
-          Generate Profile
-        </button>
-      </div>
-      <div>
-        <button
-          className="default-button long-button hover-button"
-          onClick={resetProfile}
-        >
-          Reset Profile
-        </button>
       </div>
     </div>
   );
