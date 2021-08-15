0ec0183b87c2f07885b3f1d6217a96c58ef1ee73
Matthew Nieva <matthewcn56@gmail.com>
2021-08-14 22:59:39 -0700

    set-up aws-docker-endpoint

---
+++ cloud-models/gender-word-embeddings/handler.py
@@ -0,0 +1,69 @@
+import json
+from gensim.models import KeyedVectors
+import os
+import pickle
+import boto3
+
+
+def lambda_handler(event, context):
+    """
+    Parameters
+    ----------
+    event: dict, required
+        API Gateway Lambda Proxy Input Format
+
+        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+
+    context: object, required
+        Lambda Context runtime methods and attributes
+
+        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
+
+    Returns
+    ------
+    API Gateway Lambda Proxy Output Format: dict
+
+        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+    """
+
+    WORD_WEIGHT = 100
+    word = event['word']
+    if word is None:
+        return {
+        'statusCode':400,
+        'body': json.dumps(
+            {
+                "message": "Invalid word",
+            }
+        )
+        }
+    # model = KeyedVectors.load("s3://bias-by-us-nlp-models/college-models/word_embedding_model.kv")
+    # model = pickle.load(open("s3://bias-by-us-nlp-models/college-models/word_embedding_model.p", "rb"))
+    s3 =boto3.resource('s3')
+    model = pickle.loads(s3.Bucket("bias-by-us-nlp-models").Object("college-models/word_embedding_model.p").get()['Body'].read())
+    #default score
+    try: 
+        word_index = model.key_to_index[word]
+        result = model.similarity('man', word)
+        score = result * WORD_WEIGHT
+        return {
+        'statusCode':200,
+        'body': json.dumps(
+            {
+                "message": score,
+            }
+        )
+        }
+    except KeyError:
+        score =0
+        return {
+        'statusCode':400,
+        'body': json.dumps(
+            {
+                "message": "Invalid word",
+            }
+        )
+        }
+    
+
+    
