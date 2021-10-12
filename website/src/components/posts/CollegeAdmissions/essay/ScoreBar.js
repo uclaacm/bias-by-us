6e398b17883eb424896fe752d3a902f10e8f9098
nareha <nareh@Narehs-MacBook-Pro.local>
2021-10-11 23:08:40 -0700

    Added scrollbar mechanic to activity

--- website/src/components/posts/CollegeAdmissions/essay/ScoreBar.js
+++ website/src/components/posts/CollegeAdmissions/essay/ScoreBar.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Slider from "rc-slider";
+import "rc-slider/assets/index.css";
 
 export default function ScoreBar(props) {
   return (
-    <div>The score is {props.essayScore.toFixed(2)} (Will be a bar later)</div>
+    <div>
+      The score is {props.essayScore.toFixed(2)}
+      <Slider value={props.essayScore.toFixed(2)} />
+    </div>
   );
 }
