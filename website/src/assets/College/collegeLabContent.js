c61ccb858efa0fb56cae157294155eb8e2780e18
charlenehuang1 <155873161+charlenehuang1@users.noreply.github.com>
2024-06-02 21:27:23 -0700

    draft 1 (#93)

--- website/src/assets/College/collegeLabContent.js
+++ website/src/assets/College/collegeLabContent.js
@@ -103,4 +103,63 @@ export default [
       ],
     },
   },
+  {
+    post: {
+      profilePic: teachLogo,
+      profilePicName: "Profile Picture - Facial Recognition",
+      header: "Learning from Biased Decisions",
+      headerLink: true,
+      linkTo: "facebook",
+      subheader: "What served as the source for bias in GRADE?",
+      bodyText: [
+        {
+          body: (
+            <p className="bold">
+              While GRADE greatly reduced the admission office's workload, the
+              ML model's decisions were problematic as they expressed heavy bias
+              towards male applicants. This is because{" "}
+              <u>
+                the gender distribution in UT Austin's engineering department
+                has been historically skewed towards men,
+              </u>{" "}
+              with less than a quarter of graduate students being women.{" "}
+              <br></br> <br></br>
+              These past decisions served as{" "}
+              <mark className="blue highlight">tainted examples</mark> for the
+              model's training dataset, since such decisions were{" "}
+              <b>
+                heavily affected by human bias and do not accurately represent
+                an applicant's competence.{" "}
+              </b>
+            </p>
+          ),
+        },
+      ],
+    },
+  },
+  {
+    commentSection: {
+      profilePic: teachLogo,
+      profilePicName: "Teach Logo",
+      followups: [
+        {
+          questionHeader: "Reflection Question 1:",
+          question: "In what other situations would we find tainted examples?",
+          answers: [
+            <p>
+              If a model was taught to predict employee performance through past
+              data of annual review scores, then what the actual model would do
+              is predicting
+              <mark className="bold">
+                how the manager perceives employee performance rather than their
+                actual performance.
+              </mark>{" "}
+              For example, sports fans would be more likely to buy merch for the
+              team(s) they root for as opposed to any random team.
+            </p>,
+          ],
+        },
+      ],
+    },
+  },
 ];
