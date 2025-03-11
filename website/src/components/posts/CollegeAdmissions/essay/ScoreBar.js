2e51e5fe917c6fffea7678e8e216de4c30e01a87
ihong0 <130017542+ihong0@users.noreply.github.com>
2025-03-10 19:57:36 -0700

    Add score bar (#105)

--- website/src/components/posts/CollegeAdmissions/essay/ScoreBar.js
+++ website/src/components/posts/CollegeAdmissions/essay/ScoreBar.js
@@ -1,7 +1,15 @@
 import React from "react";
+import "./essay.css";
 
 export default function ScoreBar(props) {
+  const scale = 2;
+  const shift = 0;
+  let scaledScore = props.essayScore * scale + shift;
+  scaledScore = scaledScore < 0 ? 0 : scaledScore > 100 ? 100 : scaledScore;
+
   return (
-    <div>The score is {props.essayScore.toFixed(2)} (Will be a bar later)</div>
+    <div class="score-bar">
+      <div class="score-bar-circle" style={{ left: scaledScore + "%" }}></div>
+    </div>
   );
 }
