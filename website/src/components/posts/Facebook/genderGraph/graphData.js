ab7292f5cfc6a56156f3bf572a60502faa345129
Rahul Khanna <rahul_khn@outlook.com>
2025-03-10 21:43:40 -0700

    merge fixing

--- website/src/components/posts/Facebook/genderGraph/graphData.js
+++ website/src/components/posts/Facebook/genderGraph/graphData.js
@@ -1,11 +1,15 @@
-const percentages = [15, 13, 19, 30, 37, 30, 28, 28, 23, 18, 18, 20];
-let year = 1960;
-const data = percentages.map((p) => {
-  year += 5; // years will range from 1965 to 2020
-  return {
-    name: year,
-    "women enrolled": p,
-  };
-});
+const data = [
+  { year: 1990, percent: 10, number: 195 },
+  { year: 1992, percent: 12, number: 247 },
+  { year: 1994, percent: 15, number: 279 },
+  { year: 1996, percent: 14, number: 248 },
+  { year: 1998, percent: 16, number: 279 },
+  { year: 2000, percent: 19, number: 342 },
+  { year: 2002, percent: 18, number: 369 },
+  { year: 2004, percent: 20.8, number: 409 },
+  { year: 2006, percent: 19, number: 395 },
+  { year: 2008, percent: 21.2, number: 469 },
+  { year: 2010, percent: 23.6, number: 497 },
+];
 
 export default data;
